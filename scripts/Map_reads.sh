a098e09c9a0832b65cf8dd293acd24c5f22e1ef5
mmerkin <163770255+mmerkin@users.noreply.github.com>
2024-11-04 14:53:52 +0000

    Update Map_reads.sh

--- scripts/Map_reads.sh
+++ scripts/Map_reads.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-REF=/pub64/mattm/velocity/Aphantopus_hyperantus/reference/Ahyperantus_genome.fa
-datapath=/pub61/florat/velocity/aphantopus_hyperantus/modern_data/00_raw_reads_modc/Samples
-output=/pub64/mattm/velocity/Aphantopus_hyperantus/modc_mapping
-threads=32
+REF=
+datapath=
+output=
+threads=
 
 for file in "$datapath"/*.veladapt.clean_R1.fastq.gz; do 
 filetag=$(basename "$file" ".veladapt.clean_R1.fastq.gz")
