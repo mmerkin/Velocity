6a58d47bcc7d9a7b3e80c665e851cf85727c14ca
mmerkin <163770255+mmerkin@users.noreply.github.com>
2024-11-04 15:08:06 +0000

    Update Map_reads.sh

--- scripts/Map_reads.sh
+++ scripts/Map_reads.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
+# Frequently changed variables
+
 REF=
 datapath=
 output=
 threads=
+
+
+# Exits the code if variables aren't set
+
+if [[ -z $REF ]]; then
+echo "No reference detected. Make sure to define the variables before running the script" 
+exit 1
+fi
+
+# Maps trimmed reads to reference
 
 for file in "$datapath"/*.veladapt.clean_R1.fastq.gz; do 
 filetag=$(basename "$file" ".veladapt.clean_R1.fastq.gz")
