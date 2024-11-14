2d5b7b4e11b6d00b68898cf3fcda57dc5a801f3d
mmerkin <163770255+mmerkin@users.noreply.github.com>
2024-11-14 20:59:24 +0000

    Update Atlas_prep.sh

--- scripts/Atlas_prep.sh
+++ scripts/Atlas_prep.sh
@@ -1,31 +1,31 @@
 #! /bin/bash
 
-datapath=
-output=
-readGroup=
-seqCycles=
-ATLAS=
+datapath=~/velocity/Aphantopus_hyperantus/modc_realign
+output=~/velocity/Aphantopus_hyperantus/modc_final
+readGroup=E3modc
+seqCycles=151
 
 
 ## Code
 
-mkdir -p $output
 
-echo -e "readGroup\tseqType\tseqCycles\n${readGroup}\tpaired\t${seqCycles}" > "${readGroup}_RGS.txt"
 
-for file in "$datapath"/*realn.bam; do 
+echo -e "readGroup\tseqType\tseqCycles\n$readGroup\tpaired\t$seqCycles" > "${readGroup}_RGS.txt"
+
+for file in "$datapath"/*/*realn.bam; do 
 filename=${file##*/}
 filetag=$(basename "$filename" ".bam")
 reduced_filetag=$(basename "$filename" ".realn.bam")
+mkdir -p $output/$reduced_filetag
 
-$ATLAS trimSoftClips --bam $file
+atlas trimSoftClips --bam $file
 
-$ATLAS mergeOverlappingReads \
+atlas mergeOverlappingReads \
 --readGroupSettings "${readGroup}_RGS.txt" \
 --removeSoftClippedBases \
---bam "${datapath}/${filetag}_softClippedBasesRemoved.bam"
+--bam "$datapath/$reduced_filetag/${filetag}_softClippedBasesRemoved.bam"
 
-cp "${datapath}/${filetag}_softClippedBasesRemoved_merged.bam" "$output/${reduced_filetag}_final.bam"
-cp "${datapath}/${filetag}_softClippedBasesRemoved_merged.bam.bai" "$output/${reduced_filetag}_final.bam.bai"
+cp "${datapath}/$reduced_filetag/${filetag}_softClippedBasesRemoved_merged.bam" "$output/$reduced_filetag/${reduced_filetag}_final.bam"
+cp "${datapath}/$reduced_filetag/${filetag}_softClippedBasesRemoved_merged.bam.bai" "$output/$reduced_filetag/${reduced_filetag}_final.bam.bai"
 
 done
