e4d7daceae80215a6de60495fc9d29ced6953ea8
mmerkin <163770255+mmerkin@users.noreply.github.com>
2025-01-16 15:14:59 +0000

    Create Merge_readgroups.sh

---
+++ scripts/Merge_readgroups.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+all_files=
+reseq_files=
+output=
+species_tag=
+
+mkdir -p $output
+
+
+# Extract the year and id, then copy and rename each file
+
+for i in $all_files/*/*.bam; do 
+reseq=$(echo $i | awk -F'-' '{print $4 "-" $5}' | awk -F'_' '{print $1}')
+cp $i $output/${species_tag}${reseq}.bam
+done
+
+# Merge files with the same year and id 
+
+for i in $reseq_files/*/*.bam; do
+reseq=$(echo $i | awk -F'-' '{print $4 "-" $5}' | awk -F'_' '{print $1}')
+bamaddrg -b $all_files/*${reseq}*/*.bam -b $reseq_files/*${reseq}*/*.bam > $output/${species_tag}${reseq}.bam
+done
