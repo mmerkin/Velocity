a1aa9ce82f1871b119ad6554cc13a648f18b914c
mmerkin <163770255+mmerkin@users.noreply.github.com>
2025-01-23 10:47:54 +0000

    Create merge_museum_extra_readgroups.sh

---
+++ scripts/merge_museum_extra_readgroups.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+all_files=
+reseq_files=
+reseq2_files=
+output=
+species_tag=
+
+mkdir -p $output
+
+
+# Extract the year and id, then copy and rename each file
+
+for i in $all_files/*/*.bam; do 
+reseq=$(echo $i | awk -F'-' '{print $6 "-" $7}' | awk -F'_' '{print $1}')
+echo "Moving to: $reseq" 
+cp $i $output/${species_tag}${reseq}.bam
+done
+
+# Merge files with the same year and id 
+
+for i in $reseq_files/*/*.bam; do
+reseq=$(echo $i | awk -F'-' '{print $6 "-" $7}' | awk -F'_' '{print $1}')
+bamaddrg -b $all_files/*${reseq}*/*.bam -b $reseq_files/*${reseq}*/*.bam -b $reseq2_files/*${reseq}*/*.bam > $output/${species_tag}${reseq}.bam
+done
