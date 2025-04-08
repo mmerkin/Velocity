847c5ecb315f883b562f6626d0b2c4b3c4e40879
mmerkin <163770255+mmerkin@users.noreply.github.com>
2025-04-08 22:18:51 +0100

    Update Match_MEs.sh

--- scripts/Match_MEs.sh
+++ scripts/Match_MEs.sh
@@ -3,14 +3,12 @@
 A_urticae_path="A_urticae_merian_elements.tsv"
 input_path="A_agestis_merian_elements.tsv"
 
-
-# Function to process either the lowest or highest values
-process_values() {
+match_MEs() {
 
 # Create the table header
 echo -e "ME\tCHROM\tStart\tEnd\tDirection"
 
-  # Loop through M1 to M31
+  # Loop through M1-M31
   for i in {1..31}; do
     ME="M$i"
     
@@ -24,7 +22,7 @@ echo -e "ME\tCHROM\tStart\tEnd\tDirection"
     last_gene_pos=$(awk -v me="$ME" -v mode="$modal_chr" '$2 == me && $3 == mode {print $4}' ${A_urticae_path} | sort -nr | head -n 1)
     last_gene_name=$(awk -v me="$ME" -v mode="$modal_chr" -v target_gene="$last_gene_pos" '$2 == me && $3 == mode && $4 == target_gene {print $1}' ${A_urticae_path})
     
-    # Add the output table rows
+    # Calculate output table rows 
     output_chr=$(awk -v first_gene="$first_gene_name" '$1 == first_gene {print $3}' ${input_path})
     output_first=$(awk -v first_gene="$first_gene_name" '$1 == first_gene {print $4}' ${input_path})
     output_last=$(awk -v last_gene="$last_gene_name" '$1 == last_gene {print $5}' ${input_path})
@@ -37,7 +35,7 @@ echo -e "ME\tCHROM\tStart\tEnd\tDirection"
     output_last="NA"
     fi
     
-    # Determine direction
+    # Determine direction of the ME
     if [[ "$output_first" != "NA" && "$output_last" != "NA" ]]; then
       if [ "$output_first" -lt "$output_last" ]; then
         direction="forward"
@@ -53,29 +51,27 @@ echo -e "ME\tCHROM\tStart\tEnd\tDirection"
     
   done
 
-  # Now, handle MZ separately
+  # Repeat for MZ
   modal_chr_MZ=$(awk '$2 == "MZ" {print $3}' ${A_urticae_path} | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
   
     first_gene_pos=$(awk -v me="MZ" -v mode="$modal_chr_MZ" '$2 == me && $3 == mode {print $4}' ${A_urticae_path} | sort -n | head -n 1)
     first_gene_name=$(awk -v me="MZ" -v mode="$modal_chr_MZ" -v target_gene="$first_gene_pos" '$2 == me && $3 == mode && $4 == target_gene {print $1}' ${A_urticae_path})
-    # Find busco gene at the final position in Aglais urticae
+
     last_gene_pos=$(awk -v me="MZ" -v mode="$modal_chr_MZ" '$2 == me && $3 == mode {print $4}' ${A_urticae_path} | sort -nr | head -n 1)
     last_gene_name=$(awk -v me="MZ" -v mode="$modal_chr_MZ" -v target_gene="$last_gene_pos" '$2 == me && $3 == mode && $4 == target_gene {print $1}' ${A_urticae_path})
     
-    # Add the output table rows
+
     output_chr=$(awk -v first_gene="$first_gene_name" '$1 == first_gene {print $3}' ${input_path})
     output_first=$(awk -v first_gene="$first_gene_name" '$1 == first_gene {print $4}' ${input_path})
     output_last=$(awk -v last_gene="$last_gene_name" '$1 == last_gene {print $5}' ${input_path})
-    
-    # Check that gene is present
+
     if [ -z "$output_first" ]; then
     output_first="NA"
     fi
     if [ -z "$output_last" ]; then
     output_last="NA"
     fi
-    
-    # Determine direction
+
     if [[ "$output_first" != "NA" && "$output_last" != "NA" ]]; then
       if [ "$output_first" -lt "$output_last" ]; then
         direction="forward"
@@ -86,10 +82,9 @@ echo -e "ME\tCHROM\tStart\tEnd\tDirection"
       direction="NA"
     fi
     
-    # Add the output table rows
     echo -e "MZ\t$output_chr\t$output_first\t$output_last\t$direction"
 }
 
-process_values 
+match_MEs
 
 
